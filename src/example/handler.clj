453ea33ade7575608dcd1fef8d5a0fd2ba7f1587
Jarppe <jarppe@metosin.fi>
2016-11-24 22:20:56 +0200

    finally, it works

--- src/example/handler.clj
+++ src/example/handler.clj
@@ -17,14 +17,16 @@
 
 (defnk ^:query ping
   []
-  (println "ping")
-  [[:effect/response {:ping "pong"}]])
+  (println "ping!")
+  [[:effect/response {:ping "pong"}]
+   [:effect/response-message "pong"]])
 
 (s/defschema User
   {:email s/Str})
 
 (defnk ^:command register-user
   [data :- User]
+  (println "register-user!" data)
   [[:effect/db [[:db/add -1 :user/email (:email data)]]]
    [:effect/email :registeration-email]
    [:effect/response {:code :ok}]])
@@ -37,22 +39,27 @@
 
 (defnk ^:effect db
   [data :as ctx]
-  (println "Updating db:" data)
+  (println "effect: updating db:" data)
   (assoc ctx :after-db {:tx 112233
                         :new-user-id 123456}))
 
 (defnk ^:effect email
-  [data :- s/Keyword [:after-db new-user-id :- Long] :as ctx]
-  (let [{:keys [to message]} (case data
-                               :registeration-email
-                               {:to "foo"
-                                :message (format "Your new home http://foo.bar/user/%d" new-user-id)})]
-    (println "Sending email ->" to ":" message))
+  [data :- s/Keyword
+   [:after-db new-user-id :- Long]
+   :as ctx]
+  (println "effect: sending email: template:" data "user-id:" new-user-id)
   ctx)
 
 (defnk ^:effect response
   [data :as ctx]
+  (println "effect: response" data)
   (assoc ctx :response {:status 200 :body data}))
+
+(defnk ^:effect response-message
+  [data [:sente id send-fn] :as ctx]
+  (println "effect: response-message" data send-fn)
+  (send-fn [id data])
+  ctx)
 
 ;;
 ;; ========================================================================
@@ -61,19 +68,18 @@
 ;;
 
 (def effect-interceptor
-  {:enter (fn [ctx] ctx)
-   :leave (fn [{response :response :as ctx}]
+  {:leave (fn [{:keys [response effects-dispatcher] :as ctx}]
             (if (vector? response)
-              (do (println "effects:" response)
-                  (reduce (fn [ctx [k effect]]
-                            (k/invoke (k/get-dispatcher ctx) k (assoc ctx :data effect)))
-                          ctx
-                          response))
-              (do (println "no-fx" response)
-                  ctx)))
+              (reduce (fn [ctx [effect-id effect-data]]
+                        (if (-> effects-dispatcher :handlers effect-id)
+                          (k/invoke effects-dispatcher effect-id (assoc ctx :data effect-data))
+                          ctx))
+                      ctx
+                      response)
+              ctx))
    :error (fn [ctx error]
+            (println "effect-interceptor: error" error)
             (assoc ctx :kekkonen.interceptor/queue error))})
-
 
 ;;
 ;; ========================================================================
@@ -81,13 +87,22 @@
 ;; ========================================================================
 ;;
 
-(defn make-kekkonen-config [state]
-  {:handlers {:effect [#'db #'email #'response]
-              :api [#'register-user #'ping]}
-   :meta {::roles security/require-roles}
-   :context state
-   :interceptors [security/api-key-authenticator
-                  effect-interceptor]})
+(defn make-effects-dispatcher [handlers]
+  (k/dispatcher {:type-resolver (k/type-resolver :effect)
+                 :handlers (select-keys handlers [:effect])}))
+
+(defn concatv [& v]
+  (vec (apply concat v)))
+
+(defn make-kekkonen-config [state common-handlers & context-effect-handlers]
+  (let [handlers (update common-handlers :effect concatv context-effect-handlers)]
+    (println "=>" handlers)
+    {:handlers (dissoc handlers :effect)
+     :type-resolver (k/type-resolver :command :query)
+     :meta {::roles security/require-roles}
+     :context (assoc state :effects-dispatcher (make-effects-dispatcher handlers))
+     :interceptors [security/api-key-authenticator
+                    effect-interceptor]}))
 
 ;;
 ;; ========================================================================
@@ -95,18 +110,19 @@
 ;; ========================================================================
 ;;
 
+(def handlers {:effect [#'db #'email]
+               :api [#'register-user #'ping]})
+
 (defnk create-ring-handler [state]
   (ka/api
-    (kc/deep-merge-map-like
-      {:core (make-kekkonen-config state)}
-      {:swagger {:ui "/"
-                 :spec "/swagger.json"
-                 :data {:info {:title "Example API"}}}
-       :core {:type-resolver (k/type-resolver :command :query :effect)}
-       :ring {:types {:query {:methods #{:get}
-                              :parameters {[:data] [:request :query-params]}}
-                      :command {:methods #{:post}
-                                :parameters {[:data] [:request :body-params]}}}}})))
+    {:core (make-kekkonen-config state handlers #'response)
+     :swagger {:ui "/"
+               :spec "/swagger.json"
+               :data {:info {:title "Example API"}}}
+     :ring {:types {:query {:methods #{:get}
+                            :parameters {[:data] [:request :query-params]}}
+                    :command {:methods #{:post}
+                              :parameters {[:data] [:request :body-params]}}}}}))
 
 ;;
 ;; ========================================================================
@@ -114,28 +130,22 @@
 ;; ========================================================================
 ;;
 
-(defn sente-event->request [sente-event]
-  ; TODO
-  {:request sente-event})
+(defn sente-event->ctx [sente-event]
+  {:data (-> sente-event :event second)
+   :sente sente-event})
 
 (defn create-sente-handler [state]
-  (let [dispatcher (k/dispatcher (-> (make-kekkonen-config state)
-                                     (assoc :type-resolver (k/type-resolver :command :query :effect))))]
+  (let [dispatcher (k/dispatcher (make-kekkonen-config state handlers #'response-message))]
     (fn [{action :id :as sente-event}]
-      (do (println "handling event:" action)
-          (->> (sente-event->request sente-event)
-               (k/dispatch dispatcher :invoke action))))))
+      (k/invoke dispatcher action (sente-event->ctx sente-event)))))
 
 (comment
   (let [sente-handler (create-sente-handler {})]
-    (sente-handler {:id :api/ping})))
+    (sente-handler {:id :api/ping
+                    :event [nil nil]
+                    :send-fn (fn [data] (println "sente: send response" data))}))
 
-
-(comment
   (let [sente-handler (create-sente-handler {})]
     (sente-handler {:id :api/register-user
-                    :body {:email "baba"}})))
-
-
-
-
+                    :event [nil {:email "baba"}]
+                    :send-fn (fn [data] (println "sente: send response" data))})))
